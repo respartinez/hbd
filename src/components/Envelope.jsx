489e71f7daa638189e1de6ec6d00222824ec9202
respartinez <your_actual_email@example.com>
2026-04-19 17:51:01 +0800

    update envelope

--- src/components/Envelope.jsx
+++ src/components/Envelope.jsx
@@ -5,20 +5,24 @@ function Envelope() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="envelope-wrapper" onClick={() => setOpen(!open)}>
-      <div className={`envelope ${open ? "open" : ""}`}>
-        <div className="flap">
-          <div className="flap-inner"></div>
-          <div className="flap-cover"></div>
-        </div>
-        <div className="env-body">
-          <div className="card-msg">
-            <p>Happy Birthday lods!</p>
-            <p>Enjoy your day!! 🎉🎂🎁</p>
+    <div className="main-container">
+      <div className="envelope-container">
+        <div className="envelope-wrapper" onClick={() => setOpen(!open)}>
+          <div className={`envelope ${open ? "open" : ""}`}>
+            <div className="flap">
+              <div className="flap-inner"></div>
+              <div className="flap-cover"></div>
+            </div>
+            <div className="env-body">
+              <div className="card-msg">
+                <p>Happy Birthday lods!</p>
+                <p>Enjoy your day!! 🎉🎂🎁</p>
+              </div>
+            </div>
           </div>
+          <p className="hint">{open ? "click to close" : "click to open"}</p>
         </div>
       </div>
-      <p className="hint">{open ? "click to close" : "click to open"}</p>
     </div>
   );
 }
