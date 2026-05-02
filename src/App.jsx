04b82e4c943f1b46fea01e945e53f772e2be6f9f
respartinez <your_actual_email@example.com>
2026-05-02 14:41:22 +0800

    Set QR code as default route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import QRCodeComponent from "./components/QRCodeComponent";
 import PinEntry from "./components/PinEntry";
 import Envelope from "./components/Envelope";
@@ -7,8 +7,7 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/pin" replace />} />
-        <Route path="/qr" element={<QRCodeComponent />} />
+        <Route path="/" element={<QRCodeComponent />} />
         <Route path="/pin" element={<PinEntry />} />
         <Route path="/envelope" element={<Envelope />} />
       </Routes>
